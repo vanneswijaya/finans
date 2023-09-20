9572eacfaa19d31edb64f14fc991fc89fd756f0e
vanneswijaya <vannesoey@gmail.com>
2023-09-20 23:20:56 +0800

    connected api with client

--- api/app.py
+++ api/app.py
@@ -4,20 +4,20 @@ app = Flask(__name__)
 
 usersList = ['Aaron', 'Bianca', 'Cat', 'Danny', 'Elena']
 
-@app.route('/users', methods=['GET'])
+@app.route('/api/users', methods=['GET'])
 def users():
     return jsonify({ 'users': [user for user in usersList] })
 
-@app.route('/user/<int:id>', methods=['GET'])
+@app.route('/api/user/<int:id>', methods=['GET'])
 def userById(id):
     return jsonify({ 'username': usersList[id]  })
 
-@app.route('/user/<string:name>', methods=['GET'])
+@app.route('/api/user/<string:name>', methods=['GET'])
 def getUserByName(name):
     # Show some user information
     return "Some info"
 
-@app.route('/user/<string:name>', methods=['POST'])
+@app.route('/api/user/<string:name>', methods=['POST'])
 def addUserByName(name):
     usersList.append(name)
     return jsonify({ 'message': 'New user added'  })
--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = () => {
+  const rewrites = () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:5000/api/:path*",
+      },
+    ];
+  };
+  return {
+    rewrites,
+  };
+};
